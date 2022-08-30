de4e2aa8b6a10e9823f4fe881f8f0c4f4953d503
Aditya Poolla <55913225+adityapoolla@users.noreply.github.com>
2022-08-30 18:58:29 +0530

    Create Solution.java

---
+++ src/problem2301To2400/problem2351/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] store = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(store[ch - 'a'] > 0)
+                return ch;
+            else 
+                store[ch - 'a']++;
+        }
+        return 'A';
+    }
+}

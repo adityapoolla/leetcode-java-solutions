46a7fc842969a8186175521d876a382fe2183a07
Aditya Poolla <poollaad19@gmail.com>
2024-08-27 17:45:05 -0500

    another soln for 338

--- src/problems301To400/problem338/Solution.java
+++ src/problems301To400/problem338/Solution.java
@@ -15,4 +15,15 @@ class Solution {
         }
         return res;
     }
+    
+    
+    public int[] countBits2(int n) {
+    	int dp[] = new int[n + 1];
+    	int offset = 1;
+    	for(int i = 1; i <= n; i++) {
+    		if(offset * 2 == i) offset = i;
+    		dp[i] = 1 + dp[i - offset];
+    	}
+    	return dp;
+    }
 }

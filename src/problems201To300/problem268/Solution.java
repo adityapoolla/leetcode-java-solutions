e14f63bd30b477557b891e892535b379bc83f139
Aditya Poolla <poollaad19@gmail.com>
2024-08-28 10:38:22 -0500

    another soln for 268

--- src/problems201To300/problem268/Solution.java
+++ src/problems201To300/problem268/Solution.java
@@ -11,4 +11,15 @@ class Solution {
         
         return total - arrTotal;
     }
+    
+    private int missingNumber2(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        for(int i = 0; i <= nums.length; i++) {
+            result ^= i;
+        }
+        return result;
+    }
 }

718f6581f0e2f984a1c668b1a5bfed19d50b04f1
Aditya Poolla <poollaad19@gmail.com>
2024-08-29 23:02:06 -0500

    70

---
+++ src/problems1To100/problem70/Solution.java
@@ -0,0 +1,12 @@
+package problems1To100.problem70;
+class Solution {
+    public int climbStairs(int n) {
+        int one = 1, two = 1;
+        for(int i = 0; i < n-1; i++) {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
+}

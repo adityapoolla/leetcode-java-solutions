3e2657f9b92eadfba657402a53f6c452fe824625
Aditya Poolla <poollaad19@gmail.com>
2024-09-05 14:16:19 -0500

    min stack

---
+++ src/problems101To200/problem155/MinStack.java
@@ -0,0 +1,48 @@
+package problems101To200.problem155;
+class MinStack {
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if(head == null)
+            head = new Node(val, val, null);
+        else 
+            head = new Node(val, Math.min(head.min, val), head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

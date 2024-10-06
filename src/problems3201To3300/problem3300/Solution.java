6a1c7c1c41bcb85469c6cc5404d3b15d74895edf
Aditya Poolla <poollaad19@gmail.com>
2024-10-05 22:36:56 -0500

    another soln 3300

--- src/problems3201To3300/problem3300/Solution.java
+++ src/problems3201To3300/problem3300/Solution.java
@@ -3,7 +3,7 @@ package problems3201To3300.problem3300;
 import java.util.List;
 import java.util.stream.IntStream;
 
-class Solution {
+public class Solution {
     public int minElement(int[] nums) {
         return IntStream.of(nums).map(i -> elementSum(i)).min().orElse(0);
     }
@@ -11,5 +11,21 @@ class Solution {
     private int elementSum(int num) {
         return List.of(String.valueOf(num).split("")).stream()
         .mapToInt(i -> Integer.valueOf(i)).sum();
+    }
+}
+
+class Solution2 {
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int sum = 0;
+            while(num != 0) {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            if(sum < min) min = sum;
+        }   
+        return min;
     }
 }
